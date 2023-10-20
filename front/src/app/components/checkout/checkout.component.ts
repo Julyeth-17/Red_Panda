046c0bca170da9e29f3c77ce568d6d4679f7917e
Tatiana <tatianazu97@gmail.com>
2023-10-19 21:02:05 -0500

    ya no tengo hambre//aqui esta la primera parte del carrito falta el contador

--- front/src/app/components/checkout/checkout.component.ts
+++ front/src/app/components/checkout/checkout.component.ts
@@ -3,27 +3,71 @@ import { ProductosService } from "../../services/productos.service";
 import { Productos } from "../../models/productos";
 
 @Component({
-  selector: 'app-checkout',
-  templateUrl: './checkout.component.html',
-  styleUrls: ['./checkout.component.css']
+    selector: 'app-checkout',
+    templateUrl: './checkout.component.html',
+    styleUrls: ['./checkout.component.css']
 })
 
-export class CheckoutComponent implements OnInit{
+export class CheckoutComponent implements OnInit {
 
 
-      item! : Productos;
+    productosCarrito: any = [];
+    hayProductos!: boolean
+    precioTotal: number = 0
+    contador: any = []
 
-      constructor(private _productosService: ProductosService) {}
+    constructor(private _productosService: ProductosService) { }
 
-      ngOnInit(): void {
+    ngOnInit(): void {
 
-        this._productosService.getProducto("6529fb99e57047c044f87f96")
-          .subscribe((data:any) => {
-            this.item = data;
-            console.log(data);
+
+        if (sessionStorage.getItem("carrito") != null) {
+            this.hayProductos = true
+            this.productosCarrito = JSON.parse(sessionStorage.getItem("carrito")!)
+            console.log(this.productosCarrito)
+            this.productosCarrito.forEach((productoC: any) => {
+                this.precioTotal = this.precioTotal + productoC.precio;
+                this.contador = productoC.contador
+            });
+        } else {
+            this.hayProductos = false
+        }
+
+
+    }
+
+
+    agregarALCarrito(idProducto: string) {
+        this._productosService.getProducto(idProducto).subscribe((data: any) => {
+            let productosCarrito: any = []
+
+            if (sessionStorage.getItem("carrito") == null) {
+                data.contador = 1
+                productosCarrito.push(data)
+                sessionStorage.setItem("carrito", JSON.stringify(productosCarrito))
+            } else {
+
+                productosCarrito = JSON.parse(sessionStorage.getItem("carrito")!)
+
+                let rectificaProducto = productosCarrito.findIndex((elemento: any) => elemento._id == idProducto)
+                if (rectificaProducto == -1) {
+                    data.contador = 1
+                    productosCarrito.push(data)
+                } else {
+                    data.contador = productosCarrito[rectificaProducto].contador + 1
+                    productosCarrito[rectificaProducto] = data
+                }
+                sessionStorage.setItem("carrito", JSON.stringify(productosCarrito))
+
+
+            }
+
+            console.log(sessionStorage.getItem("carrito"));
             // localStorage.setItem('')
-          });
-}
+        });
+    }
+
+
 }
 
 
